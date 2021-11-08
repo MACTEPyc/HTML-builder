558a5412836eca0281b2f0de0a507378e77f2f1f
MACTEPyc <MACTEPyc@gmail.com>
2021-11-08 19:27:49 +0500

    feat: edit 01-read-file task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,22 +1,25 @@
 const fs = require('fs');
 const path = require('path');
-const EventEmitter = require('events');
-const { stdin, stdout } = require('process');
+const { stdout } = require('process');
 
 const fileName = 'text.txt';
 
 const pathFile = path.join(__dirname, fileName);
 
 const stream = new fs.ReadStream(pathFile, {encoding: 'utf-8'});
-stream.on('readable', () => {
-  const data = stream.read();
-  if (data != null) { console.log(data); }
-});
 
-stream.on('error', (err) => {
-  if (err.code == 'ENOENT') {
-    console.log(`файл "${fileName}" не найден`);
-  } else {
-    console.error(err);
-  }
-});
+stream.pipe(stdout);
+
+// stream.on('readable', () => {
+//   const data = stream.read();
+//   if (data != null) { console.log(data); }
+// });
+
+// stream.on('error', (err) => {
+//   if (err.code == 'ENOENT') {
+//     console.log(`файл "${fileName}" не найден`);
+//   } else {
+//     console.error(err);
+//   }
+// });
+
