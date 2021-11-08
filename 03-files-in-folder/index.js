2ec3f62a1110a67e4f3b31ea24e76e0dd3982d6b
MACTEPyc <MACTEPyc@gmail.com>
2021-11-09 03:30:47 +0500

    feat: add 03-files-in-folder task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,27 @@
+const { readdir } = require('fs').promises;
+const { stat } = require('fs');
+const path = require('path');
+
+const nameDir = 'secret-folder';
+const pathDir = path.join(__dirname, nameDir);
+
+(async function (dir) {
+  try {
+    const files = await readdir(dir, { withFileTypes: true });
+    for (const file of files) {
+      if (!file.isDirectory()) {
+        const pathFile = path.join(pathDir, file.name);
+        console.log(pathFile);
+        stat(pathFile, (err, stats) => {
+          const extName = path.extname(file.name);
+          const fileName = path.basename(pathFile, extName);
+          const size = stats.size;
+          console.log(`${fileName} - ${extName.slice(1)} - ${size}kb`);
+        });
+      }
+  }
+  } catch(err) {
+    console.error(err);
+  }
+})(pathDir);
+
